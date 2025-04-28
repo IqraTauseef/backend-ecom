6cb2662d900b55f5e5c3a11a57e4b0c565226e02
IqraTauseef <156837674+IqraTauseef@users.noreply.github.com>
2025-04-29 02:51:30 +0500

    removing extra space

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ connectDB();
 
 app.use(
     cors({
-        origin: ' http://localhost:5173',
+        origin: 'http://localhost:5173',
         credentials: true,
         methods:['GET','POST','PUT','DELETE'],
         allowedHeaders:[
